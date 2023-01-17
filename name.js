4c899037eeabfa918dc3314c34cbc2d94479d7fa
Md. Aminur Rahman <118046704+aminurr395@users.noreply.github.com>
2023-01-17 18:16:36 +0600

    Add files via upload

---
+++ name.js
@@ -0,0 +1,10 @@
+<script>
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+</script>
